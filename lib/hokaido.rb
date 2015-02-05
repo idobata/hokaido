7505c99fb3af74f25cfe8d6dd738d0dbcce16c41
ursmhbry <ursmhbry@ursm.jp>
2015-02-05 12:02:47 +0900

    hi go

--- lib/hokaido.rb
+++
@@ -1,8 +0,0 @@
-require 'hokaido/version'
-
-module Hokaido
-  autoload :Broadcast, 'hokaido/broadcast'
-  autoload :CLI,       'hokaido/cli'
-  autoload :Server,    'hokaido/server'
-  autoload :Watcher,   'hokaido/watcher'
-end
--- lib/hokaido/version.rb
+++
@@ -1,3 +0,0 @@
-module Hokaido
-  VERSION = '0.0.7'
-end
